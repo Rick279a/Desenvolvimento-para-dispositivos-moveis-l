4716305be612674390039d5eb32a3a32b6565504
Ricardo Pereira <ricardomp279@gmail.com>
2024-03-22 13:13:33 -0300

    ...

---
+++ Exemplos em Aula 1 Introdução em flutter/exemplo1/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(Container(
+    child: Center(
+      child: const Text(
+        'olá Pessoal',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.red, fontSize: 50.0),
+      ),
+    ),
+  ));
+}
